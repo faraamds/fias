5c21d843d2bc5fcafa0c312bb3de5a6c9b954899
Андрей Рассказов <arasskazov@bis.ru>
2019-12-08 19:55:50 +0300

    Создание вспомогательной таблицы вынесено в процедуру

--- src/database/procedures/postgresql/fias_address_fill_help_search_table_address_full.sql
+++ src/database/procedures/postgresql/fias_address_fill_help_search_table_address_full.sql
@@ -14,33 +14,38 @@ DECLARE
 
 BEGIN
 
-    TRUNCATE TABLE fias_address_object_help_search;
-    DROP INDEX IF EXISTS idnx_fias_address_object_help_search_address;
-    DROP INDEX IF EXISTS idnx_fias_address_object_help_search_aoguid;
-    DROP INDEX IF EXISTS idnx_fias_address_object_help_search_regioncode;
-    DROP INDEX IF EXISTS indx_fias_address_object_help_search_ao_count;
+    --     TRUNCATE TABLE fias_address_object_help_search;
+--     DROP INDEX IF EXISTS idnx_fias_address_object_help_search_address;
+--     DROP INDEX IF EXISTS idnx_fias_address_object_help_search_aoguid;
+--     DROP INDEX IF EXISTS idnx_fias_address_object_help_search_regioncode;
+--     DROP INDEX IF EXISTS indx_fias_address_object_help_search_ao_count;
+
+    DROP TABLE IF EXISTS fias_address_object_help_search;
+    CREATE TABLE fias_address_object_help_search
+    (aoguid UUID NOT NULL, houseguid UUID, roomguid UUID, regioncode VARCHAR(2),
+     ao_count INT, address TEXT, house VARCHAR(255), flatnumber VARCHAR(255), buildnum varchar(255));
 
     FOR var_aoguid, var_regioncode IN SELECT MIN(aoguid::TEXT), MIN(regioncode) FROM fias_address_object GROUP BY aoguid LOOP
 
-        var_search_result := fias_address_formal_whole_history_with_count(var_aoguid::UUID);
-        var_houseguid := NULL;
-        var_roomguid := NULL;
+            var_search_result := fias_address_formal_whole_history_with_count(var_aoguid::UUID);
+            var_houseguid := NULL;
+            var_roomguid := NULL;
 
-        INSERT into fias_address_object_help_search (aoguid, houseguid, roomguid, regioncode, address, ao_count)
-        VALUES (var_aoguid::UUID, var_houseguid, var_roomguid, var_regioncode, var_search_result[1], var_search_result[2]::INT);
+            INSERT into fias_address_object_help_search (aoguid, houseguid, roomguid, regioncode, address, ao_count)
+            VALUES (var_aoguid::UUID, var_houseguid, var_roomguid, var_regioncode, var_search_result[1], var_search_result[2]::INT);
 
-        FOR var_houseguid, var_roomguid, var_house, var_flatnumber, var_buildnum IN SELECT houseguid, roomguid, house, flatnumber, buildnum
-                FROM fias_house_room_tmp
-                WHERE fias_house_room_tmp.aoguid=var_aoguid::UUID LOOP
+            FOR var_houseguid, var_roomguid, var_house, var_flatnumber, var_buildnum IN SELECT houseguid, roomguid, house, flatnumber, buildnum
+                                                                                        FROM fias_house_room_tmp
+                                                                                        WHERE fias_house_room_tmp.aoguid=var_aoguid::UUID LOOP
 
-            INSERT into fias_address_object_help_search (aoguid, houseguid, roomguid, regioncode, address, ao_count, house, flatnumber, buildnum)
-            VALUES (var_aoguid::UUID, var_houseguid, var_roomguid, var_regioncode, var_search_result[1], var_search_result[2]::INT + 1, var_house, var_flatnumber, var_buildnum);
+                    INSERT into fias_address_object_help_search (aoguid, houseguid, roomguid, regioncode, address, ao_count, house, flatnumber, buildnum)
+                    VALUES (var_aoguid::UUID, var_houseguid, var_roomguid, var_regioncode, var_search_result[1], var_search_result[2]::INT + 1, var_house, var_flatnumber, var_buildnum);
 
-        END LOOP;
+                END LOOP;
 
-        var_i:=var_i+1;
+            var_i:=var_i+1;
 
-        raise notice '%', var_i;
+            raise notice '%', var_i;
 
         END LOOP;
 
