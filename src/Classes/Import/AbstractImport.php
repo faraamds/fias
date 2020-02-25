7f2b73563f55f4d38b5d65854a789d6029af97cf
Андрей Рассказов <arasskazov@bis.ru>
2020-02-25 14:02:01 +0300

    Добвалено создание доп. промежуточной таблицы

--- src/Classes/Import/AbstractImport.php
+++ src/Classes/Import/AbstractImport.php
@@ -44,9 +44,11 @@ abstract class AbstractImport
         DB::select('select * from fias_address_create_house_tmp_table()');
         echo("done\nCreating room tmp table ...");
         DB::select('select * from fias_address_create_room_tmp_table()');
-        echo("done\nDeleting fias_house_tmp table ...");
-        DB::delete('drop table fias_house_tmp');
+        echo("done\nCreating house_room tmp table ...");
+        DB::delete('select * from fias_address_create_house_room_table');
         echo("done\nDeleting fias_room_tmp table ...");
+        DB::delete('drop table fias_room_tmp');
+        echo("done\nCreating address search table ...");
         DB::delete('drop table fias_room_tmp');
         echo("done\nCreating address search table ...");
         DB::select('select * from fias_address_create_ao_tmp_table()');
